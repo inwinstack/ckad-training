e75d987b15a8eff753a6f7516b2dfb3a7043877d
jmtorres <juanmanuel.torres@guadaltech.es>
2018-10-03 18:47:42 +0200

    init solution

---
+++ juan_torres/session-01/01/commands.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+kubectl create -f namespace.yaml
+
+kubectl create secret generic secret-mariadb -n exercise-01 \
+	--from-literal=mariadb_password=$(openssl rand -hex $(echo $RANDOM %22 +32 | bc)) \
+	--from-literal=mariadb_root_password=$(openssl rand -hex $(echo $RANDOM %22 +32 | bc))
+
+kubectl create secret generic secret-wordpress -n exercise-01 \
+	--from-literal=wordpress_password=$(openssl rand -hex $(echo $RANDOM %22 +32 | bc))
+
+kubectl create -f cm.yaml
+
+kubectl create -f mariadb-svc.yaml
+kubectl create -f wordpress-svc.yaml
+
+kubectl create -f mariadb-deployment.yaml
+kubectl create -f wordpress-deployment.yaml
+sleep 10
+kubectl create -f wordpress-canary-deployment.yaml
---
+++ juan_torres/session-01/02/commands.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+kubectl create -f namespace.yaml
+
+kubectl create secret generic secret-mariadb -n exercise-02 \
+	--from-literal=mariadb_password=$(openssl rand -hex $(echo $RANDOM %12 +22 | bc)) \
+	--from-literal=mariadb_replication_password=$(openssl rand -hex $(echo $RANDOM %12 +22 | bc)) \
+	--from-literal=mariadb_root_password=$(openssl rand -hex $(echo $RANDOM %12 +22 | bc))
+
+kubectl create secret generic secret-wordpress -n exercise-02 \
+	--from-literal=wordpress_password=$(openssl rand -hex $(echo $RANDOM %22 +32 | bc))
+
+kubectl create -f cm.yaml
+
+kubectl create -f mariadb-master-svc.yaml
+kubectl create -f mariadb-slave-svc.yaml
+kubectl create -f wordpress-svc.yaml
+
+kubectl create -f mariadb-master-deployment.yaml
+sleep 5
+kubectl create -f mariadb-slave-deployment.yaml
+sleep 5
+kubectl create -f wordpress-deployment.yaml
+
+sleep 10
+kubectl scale --replicas=5 deployment/mariadb-slave -n exercise-02
+
+kubectl get deploy mariadb-slave -n exercise-02 -o json | jq '.spec.strategy.rollingUpdate.maxUnavailable = "40%"' > temp.yaml  \
+	&& kubectl replace -f temp.yaml \
+	&& rm -f temp.yaml
+	
