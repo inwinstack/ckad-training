837bd9e85b2b3c39937134e5a06c028c554b6bb2
jica <jicenteno@gmail.com>
2018-10-04 15:05:58 +0200

    ckad-trainingsession01-exercise01. Pull request comments (except readiness/liveness probes)

--- jose_centeno/session-01/01/command.sh
+++ jose_centeno/session-01/01/command.sh
@@ -4,31 +4,31 @@
 kubectl create ns exercise-01
 
 #Create an alias to set the namespace automaticaly, it can also be done on kube config.
-alias k="kubectl -n exercise-01"
+kubectl config set-context kubernetes-admin@kubernetes --namespace=exercise-01
 
 # Create the configuration 
-k create -f cm.yaml -f secret.yaml 
+kubectl create -f cm.yaml -f secret.yaml 
 
 # Create mariaDB Service
 ## Create mariaDB deployment
-k create -f mariadb-deployment.yaml
+kubectl create -f mariadb-deployment.yaml
 ### Check that mariaDB pod is correct
-k get pods
+#kubectl get pods
 ### Chech mariaDB logs (1st pod)
-k logs $(k get pods -lapp=myblog,tier=backend -o jsonpath="{.items[0].metadata.name}")
+#kubectl logs $(k get pods -lapp=myblog,tier=backend -o jsonpath="{.items[0].metadata.name}")
 ## Create mariaDB clusterIP service to be reached from wordpress
-k create -f mariadb-svc.yaml 
+kubectl create -f mariadb-svc.yaml 
 
 # Create Wordpress Service
 ## Create the deployment
-k create -f wordpress-deployment.yaml
+kubectl create -f wordpress-deployment.yaml
 ## Create the deployment of the canary
- k create -f wordpress-canary-deployment.yaml 
+kubectl create -f wordpress-canary-deployment.yaml 
 
 ### Check that te pods are ok
-k get pods -lapp=myblog,l=tier=frontend
+#kubectl get pods -lapp=myblog,l=tier=frontend
 ## Create the LoadBalancer service (it will not be able to assign an external IP)
-k create -f wordpress-svc.yaml
+kubectl create -f wordpress-svc.yaml
 
 # Delete all resources
-k delete ns exercise-01
+#kubectl delete ns exercise-01
