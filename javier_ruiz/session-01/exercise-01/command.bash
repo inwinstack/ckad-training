d15960d95f10317a8c0555d820b01e5ebcfd37c9
Javier Ruiz <japi@kolokium>
2018-10-05 15:05:37 +0200

    Fix use of kubectl instead of the k alias

--- javier_ruiz/session-01/exercise-01/command.bash
+++ javier_ruiz/session-01/exercise-01/command.bash
@@ -1,4 +1,4 @@
 kubectl apply -f exercise-01-namespace.yaml
 kubectl apply -f .
-kubectl config set-context $(k config current-context) --namespace=exercise-01
+kubectl config set-context $(kubectl config current-context) --namespace=exercise-01
 watch  'kubectl get pod -l app=wordpress -o name | while read pod; do echo; echo --- $pod; echo; kubectl logs $pod|tail -15; done'
